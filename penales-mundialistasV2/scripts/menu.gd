dc1a737ac58f973f78ad283b46d91ed1c575506d
socarros02 <amadeosocarros0@gmail.com>
2025-05-03 19:57:35 -0300

    escena fixture

--- penales-mundialistasV2/scripts/menu.gd
+++ penales-mundialistasV2/scripts/menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenas/arcade.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/fixtureMundial.tscn")
